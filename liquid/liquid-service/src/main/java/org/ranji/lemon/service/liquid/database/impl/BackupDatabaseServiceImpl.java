1358f3f25e11ac0d68d19acd5d3989810bb9ddd7
jiran1221 <jiran1221@163.com>
2017-10-19 16:14:43 +0800

    1. 把数据源由c3p0换为druid 2. 增加数据性能检测功能 3. 解决了不必要的冲突

--- liquid/liquid-service/src/main/java/org/ranji/lemon/service/liquid/database/impl/BackupDatabaseServiceImpl.java
+++ liquid/liquid-service/src/main/java/org/ranji/lemon/service/liquid/database/impl/BackupDatabaseServiceImpl.java
@@ -6,17 +6,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.ResourceBundle;
-
 import org.ranji.lemon.common.core.service.impl.GenericServiceImpl;
 import org.ranji.lemon.model.liquid.database.BackupDatabaseInfo;
 import org.ranji.lemon.service.liquid.database.prototype.IBackupDatabaseService;
-import org.springframework.context.support.AbstractApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.stereotype.Repository;
-
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 @Repository
 public class BackupDatabaseServiceImpl extends GenericServiceImpl<BackupDatabaseInfo, Integer>implements IBackupDatabaseService {
@@ -57,16 +51,7 @@ public class BackupDatabaseServiceImpl extends GenericServiceImpl<BackupDatabase
 	 * @throws IOException 
 	 */
 	private void initVariableByProperties() throws IOException{
-		/*
-		 * 1 利用类加载器获取配置文件信息
-		 */
-//		InputStream inStream = DBOperationImpl.class.getClassLoader().getResourceAsStream("config/jdbc.properties");
-//		Properties prop = new Properties();  
-//		prop.load(inStream);  
-//		userName = prop.getProperty("jdbc.username","root"); //设置默认值
-//		password = prop.getProperty("jbbc.password","root");
-//		String url = prop.getProperty("jdbc.url");
-		//2 通过java.util.ResourceBundle类来读取
+		//1  通过java.util.ResourceBundle类来读取
 		ResourceBundle resource = ResourceBundle.getBundle("config/jdbc");
 		userName = resource.getString("jdbc.username");
 		password = resource.getString("jdbc.password");
@@ -74,19 +59,5 @@ public class BackupDatabaseServiceImpl extends GenericServiceImpl<BackupDatabase
 		//获取IP地址
 		hostIP = url.split("/")[2].split(":")[0];
 	}
-	/**
-	 * 初始化数据库连接信息（通过xml文件获取）
-	 * 
-	 */
-	public void initVariableByXml(){
-		URL path = BackupDatabaseServiceImpl.class.getClassLoader().getResource("config/spring-persist.xml");  //获取xml文件路径
-		AbstractApplicationContext ac = new FileSystemXmlApplicationContext(path.toString());
-		ComboPooledDataSource dataSource =(ComboPooledDataSource) ac.getBean("dataSource");
-		ac.close();
-		userName = dataSource.getUser();
-		password = dataSource.getPassword();
-		String url = dataSource.getJdbcUrl();
-		//获取IP地址
-		hostIP = url.split("/")[2].split(":")[0];
-	}
+	
 }
