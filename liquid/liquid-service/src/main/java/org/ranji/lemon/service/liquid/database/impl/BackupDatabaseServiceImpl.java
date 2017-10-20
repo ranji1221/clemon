f30cd324db009ad1b860cbca3e159838d9e219d4
fengjie <fengjie1103@163.com>
2017-10-20 09:24:56 +0800

    1 增加数据库还原service方法

--- liquid/liquid-service/src/main/java/org/ranji/lemon/service/liquid/database/impl/BackupDatabaseServiceImpl.java
+++ liquid/liquid-service/src/main/java/org/ranji/lemon/service/liquid/database/impl/BackupDatabaseServiceImpl.java
@@ -2,10 +2,13 @@ package org.ranji.lemon.service.liquid.database.impl;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.util.ResourceBundle;
 import org.ranji.lemon.common.core.service.impl.GenericServiceImpl;
 import org.ranji.lemon.model.liquid.database.BackupDatabaseInfo;
@@ -46,6 +49,29 @@ public class BackupDatabaseServiceImpl extends GenericServiceImpl<BackupDatabase
 		reader.close();
 		inputStream.close();
 	}
+	
+	@Override
+	public void recover(String path) throws IOException {
+		initVariableByProperties();  //初始化数据库连接
+		Runtime runtime = Runtime.getRuntime();
+		Process process = runtime.exec("cmd /c mysql -h "+ hostIP +" -u "+ userName +" -p"+ password +" --default-character-set=utf8 lemon");
+		OutputStream outputStream = process.getOutputStream();
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+		String str = null;
+		StringBuffer sb = new StringBuffer();
+		while((str = br.readLine()) != null){
+			sb.append(str+"\r\n");
+		}
+		str = sb.toString();
+		System.out.println(str);
+		OutputStreamWriter writer = new OutputStreamWriter(outputStream,"utf-8");
+		writer.write(str);
+		writer.flush();
+		outputStream.close();
+		br.close();
+		writer.close();
+		
+	}
 	/**
 	 * 初始化数据库连接信息（通过properties文件获取）
 	 * @throws IOException 
@@ -59,5 +85,6 @@ public class BackupDatabaseServiceImpl extends GenericServiceImpl<BackupDatabase
 		//获取IP地址
 		hostIP = url.split("/")[2].split(":")[0];
 	}
+	
 	
 }
